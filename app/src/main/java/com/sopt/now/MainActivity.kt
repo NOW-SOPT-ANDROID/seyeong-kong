420c027b6269ee1ef48f01954b2404e6442305f6
0se0 <030ggong@gmail.com>
2024-04-02 01:32:17 +0900

    Feat: MainActivity 구현

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.sopt.now
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val id = intent.getStringExtra("id")
+        val nickname = intent.getStringExtra("nickname")
+
+        binding.tvId.text = id
+        binding.tvNickname.text = nickname
     }
 }
