0add46d710ba7f1237b07030eb50f5e9b0830b98
0se0 <030ggong@gmail.com>
2024-04-03 04:59:14 +0900

    Feat: 메인 액티비티 구현

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.sopt.now
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +16,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val id = intent.getStringExtra("id")
-        val nickname = intent.getStringExtra("nickname")
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnv.setupWithNavController(navController)
 
-        binding.tvId.text = id
-        binding.tvNickname.text = nickname
+        hideBottomNavigationView(navController)
+    }
+
+    private fun hideBottomNavigationView(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnv.visibility = when (destination.id) {
+                R.id.loginFragment -> View.GONE
+                R.id.signupFragment -> View.GONE
+                R.id.profileFragment -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
     }
 }
