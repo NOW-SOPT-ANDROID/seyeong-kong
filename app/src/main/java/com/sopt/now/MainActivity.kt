df18873d552ca29ecd6ef7343fd600aae5bf5c87
0se0 <030ggong@gmail.com>
2024-04-03 13:41:25 +0900

    Remove: 하단바 삭제

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.sopt.now
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,20 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bnv.setupWithNavController(navController)
+        navController = navHostFragment.navController
 
-        hideBottomNavigationView(navController)
-    }
-
-    private fun hideBottomNavigationView(navController: NavController) {
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.bnv.visibility = when (destination.id) {
-                R.id.loginFragment -> View.GONE
-                R.id.signupFragment -> View.GONE
-                R.id.profileFragment -> View.GONE
-                else -> View.VISIBLE
-            }
-        }
     }
 }
