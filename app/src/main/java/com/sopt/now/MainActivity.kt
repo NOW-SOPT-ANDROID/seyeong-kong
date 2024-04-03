2d86da5cacbf518e9abb3c0104fa3d0cec9a0119
0se0 <030ggong@gmail.com>
2024-04-03 16:58:53 +0900

    Feat: 메인 액티티비 mbti 추가

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -2,23 +2,22 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
-        navController = navHostFragment.navController
+        val id = intent.getStringExtra("id")
+        val nickname = intent.getStringExtra("nickname")
+        val mbti = intent.getStringExtra("mbti")
 
+        binding.tvId.text = id
+        binding.tvNickname.text = nickname
+        binding.tvMbti.text = mbti
     }
 }
