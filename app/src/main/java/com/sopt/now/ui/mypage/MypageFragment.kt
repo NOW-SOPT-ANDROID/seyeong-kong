d07c162bf840f36c49de4d4846a20f3206b917ee
0se0 <030ggong@gmail.com>
2024-04-16 22:34:18 +0900

    Rename: 기존 프래그먼트 패키지 변경

---
+++ app/src/main/java/com/sopt/now/ui/mypage/MypageFragment.kt
@@ -0,0 +1,58 @@
+package com.sopt.now.ui.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sopt.now.R
+import com.sopt.now.data.User
+import com.sopt.now.data.UserRepository
+import com.sopt.now.databinding.FragmentMypageBinding
+
+class MypageFragment : Fragment() {
+    private var _binding: FragmentMypageBinding? = null
+    private val binding: FragmentMypageBinding
+        get() = requireNotNull(_binding) { "FragmentMypageBinding is not initialized" }
+    private lateinit var userRepository: UserRepository
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userRepository = UserRepository(requireContext())
+        val user = userRepository.getUserData()
+        setUserData(user)
+        initLogoutBtnClickListener()
+    }
+
+    private fun setUserData(user: User?) {
+        user.let {
+            binding.tvId.text = it?.id
+            binding.tvMbti.text = it?.mbti
+            binding.tvNickname.text = it?.nickname
+        }
+    }
+
+    private fun initLogoutBtnClickListener() {
+        binding.tvLogout.setOnClickListener {
+            userRepository.clearUserData()
+            findNavController().navigate(R.id.action_mypage_to_login)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
