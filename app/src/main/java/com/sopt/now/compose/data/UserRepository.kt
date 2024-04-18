7226c4165134948d3c9147eae030daa8325bb8d3
0se0 <030ggong@gmail.com>
2024-04-19 00:40:24 +0900

    Feat: 자동로그인 구현 repository

---
+++ app/src/main/java/com/sopt/now/compose/data/UserRepository.kt
@@ -0,0 +1,51 @@
+package com.sopt.now.compose.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserRepository(context: Context) {
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("SaveLogin", Context.MODE_PRIVATE)
+
+    fun setUserLoggedIn(loggedIn: Boolean) {
+        sharedPreferences.edit().putBoolean("isLoggedIn", loggedIn).apply()
+    }
+
+    fun isUserLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean("isLoggedIn", false)
+    }
+
+    fun isValidateUser(inputId: String, inputPw: String): Boolean {
+        val user = getUserData()
+        return user != null && user.id == inputId && user.password == inputPw
+    }
+
+    fun getUserData(): User? {
+        val userId = sharedPreferences.getString(KEY_USER_ID, null) ?: return null
+        val password = sharedPreferences.getString(KEY_USER_PW, null) ?: return null
+        val nickname = sharedPreferences.getString(KEY_NICKNAME, null) ?: return null
+        val mbti = sharedPreferences.getString(KEY_MBTI, null) ?: return null
+        return User(userId, password, nickname, mbti)
+    }
+
+    fun saveUserData(user: User) {
+        sharedPreferences.edit().apply {
+            putString(KEY_USER_ID, user.id)
+            putString(KEY_USER_PW, user.password)
+            putString(KEY_NICKNAME, user.nickname)
+            putString(KEY_MBTI, user.mbti)
+            apply()
+        }
+    }
+
+    fun clearUserData() {
+        sharedPreferences.edit().clear().apply()
+    }
+
+    companion object {
+        private const val KEY_USER_ID = "UserID"
+        private const val KEY_USER_PW = "Password"
+        private const val KEY_NICKNAME = "Nickname"
+        private const val KEY_MBTI = "Mbti"
+    }
+}
