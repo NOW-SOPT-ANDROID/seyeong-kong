1b5abde8f68fe80f4339135447b8ad969341b5f8
0se0 <030ggong@gmail.com>
2024-04-25 17:46:13 +0900

    Refactor: 프로필 및 친구목록

--- app/src/main/java/com/sopt/now/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/now/ui/home/HomeViewModel.kt
@@ -10,87 +10,86 @@ class HomeViewModel : ViewModel() {
     val mockFriendList = listOf(
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "1",
-            selfDescription = "Sweet Child O' Mine",
+            name = "임하늘",
+            selfDescription = "나는 마늘!! 별이 달이",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "2",
-            selfDescription = "AWOLNATION - Run",
+            name = "우상욱",
+            selfDescription = "우상욱상우",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "3",
-            selfDescription = "Avril Lavigne - Sk8er Boi",
+            name = "신민석",
+            selfDescription = "신민석민식",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "4",
-            selfDescription = "TEXAS HOLD 'EM",
+            name = "임하늘",
+            selfDescription = "나는 마늘!! 별이 달이",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "5",
-            selfDescription = "Future, Metro Boomin, Kendrick Lamar - Like That",
+            name = "우상욱",
+            selfDescription = "우상욱상우",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "6",
-            selfDescription = "Oasis - Don't Look Back in Anger",
+            name = "신민석",
+            selfDescription = "신민석민식",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "7",
-            selfDescription = "Bring That Fire",
+            name = "임하늘",
+            selfDescription = "나는 마늘!! 별이 달이",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "8",
-            selfDescription = "OneRepublic - Run",
+            name = "우상욱",
+            selfDescription = "우상욱상우",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "9",
-            selfDescription = "Kate Bush - Running Up That Hill",
+            name = "신민석",
+            selfDescription = "신민석민식",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "10",
-            selfDescription = "bbno$ - Edamame",
+            name = "임하늘",
+            selfDescription = "나는 마늘!! 별이 달이",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "11",
-            selfDescription = "Let’s Get The Party Started (feat. Bring Me The Horizon)",
+            name = "우상욱",
+            selfDescription = "우상욱상우",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "12",
-            selfDescription = "SVEA - All My Exes",
+            name = "신민석",
+            selfDescription = "신민석민식",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "13",
-            selfDescription = "Everyday Feels Like Sunday",
+            name = "임하늘",
+            selfDescription = "나는 마늘!! 별이 달이",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "14",
-            selfDescription = "24KGoldn, iann dior - Mood",
+            name = "우상욱",
+            selfDescription = "우상욱상우",
         ),
         Friend(
             profileImg = R.drawable.ic_mypage,
-            name = "15",
-            selfDescription = "Stacey Ryan - Fall In Love Alone",
+            name = "신민석",
+            selfDescription = "신민석민식",
         ),
     )
 
     val mockProfileList = listOf(
         Profile(
             profileImg = R.drawable.ic_mypage,
-            name = "0se0",
+            name = "공세영",
             selfDescription = "NOW SOPT ANDROID NOW SOPT ANDROID NOW SOPT ANDROID NOW SOPT ANDROID NOW SOPT ANDROID NOW SOPT ANDROID NOW SOPT ANDROID",
         ),
     )
-
 }
