923efc527c7d008998c5d853aa0e2edbd263910b
0se0 <030ggong@gmail.com>
2024-04-25 17:48:21 +0900

    Refactor: 네비게이션 프래그먼트 에러 핸들링

--- app/src/main/java/com/sopt/now/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/now/ui/main/MainActivity.kt
@@ -6,28 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.R
 import com.sopt.now.data.UserRepository
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var userRepository: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        userRepository = UserRepository(this)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.main_fcv) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_fcv) as? NavHostFragment
+        if (navHostFragment != null) {
+            val navController = navHostFragment.navController
+            setupNavigation(navController)
+        } else {
+            Snackbar.make(binding.root, "NavigationError", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupNavigation(navController: NavController) {
         binding.mainBnv.setupWithNavController(navController)
-
         hideBottomNavigationView(navController)
 
-        if (userRepository.isUserLoggedIn()) {
+        if (UserRepository.isUserLoggedIn()) {
             navController.navigate(R.id.homeFragment)
         } else {
             navController.navigate(R.id.loginFragment)
