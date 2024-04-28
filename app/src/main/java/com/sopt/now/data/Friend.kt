a23583baedfa1b1bfae0519d14a8440c32657776
0se0 <030ggong@gmail.com>
2024-04-28 17:44:19 +0900

    Feat: 식별 아이디 추가

--- app/src/main/java/com/sopt/now/data/Friend.kt
+++ app/src/main/java/com/sopt/now/data/Friend.kt
@@ -3,6 +3,7 @@ package com.sopt.now.data
 import androidx.annotation.DrawableRes
 
 data class Friend(
+    var id: Int,
     @DrawableRes val profileImg: Int,
     val name: String,
     val selfDescription: String
