7de89d66c831c8b0133fce985527570e5ccb4e36
0se0 <030ggong@gmail.com>
2024-04-29 02:54:53 +0900

    Feat: FriendDao

---
+++ app/src/main/java/com/sopt/now/data/FriendDao.kt
@@ -0,0 +1,19 @@
+package com.sopt.now.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FriendDao {
+    @Insert
+    suspend fun insertFriend(friend: Friend)
+
+    @Delete
+    suspend fun deleteFriend(friend: Friend)
+
+    @Query("SELECT * from friend_table ORDER BY id ASC")
+    fun getAllFriends(): Flow<List<Friend>>
+}
