1489092f81febbaf15d91d8366d16e9ec161c92c
0se0 <030ggong@gmail.com>
2024-04-29 02:53:51 +0900

    Feat: Entity 클래스

--- app/src/main/java/com/sopt/now/data/Friend.kt
+++ app/src/main/java/com/sopt/now/data/Friend.kt
@@ -1,10 +1,13 @@
 package com.sopt.now.data
 
 import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "friend_table")
 data class Friend(
-    var id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     @DrawableRes val profileImg: Int,
     val name: String,
-    val selfDescription: String
+    val selfDescription: String? = null,
 )
