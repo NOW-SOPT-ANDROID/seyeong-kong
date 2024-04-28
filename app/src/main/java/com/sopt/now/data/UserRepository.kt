8857974c0ef1186b66f93b1254c38d2f89b1e5f6
0se0 <030ggong@gmail.com>
2024-04-28 20:26:32 +0900

    Feat: userRepository 클래스로 변경

--- app/src/main/java/com/sopt/now/data/UserRepository.kt
+++ app/src/main/java/com/sopt/now/data/UserRepository.kt
@@ -1,31 +1,26 @@
 package com.sopt.now.data
 
-import android.content.Context
 import android.content.SharedPreferences
 
-class UserRepository(context: Context){
-    private var sharedPreferences: SharedPreferences =
-        context.getSharedPreferences("save_login", Context.MODE_PRIVATE)
-
-    fun init(context: Context) {
-        sharedPreferences =
-            context.applicationContext.getSharedPreferences("SaveLogin", Context.MODE_PRIVATE)
+class UserRepository(private val preferences: SharedPreferences) {
+    fun setUserLoggedIn(loggedIn: Boolean) {
+        preferences.edit().putBoolean("isLoggedIn", loggedIn).apply()
     }
 
-    fun setUserLoggedIn(loggedIn: Boolean) {
-        sharedPreferences.edit().putBoolean("isLoggedIn", loggedIn).apply()
+    fun isUserLoggedIn(): Boolean {
+        return preferences.getBoolean("isLoggedIn", false)
     }
 
     fun getUserData(): User? {
-        val userId = sharedPreferences.getString(KEY_USER_ID, null) ?: return null
-        val password = sharedPreferences.getString(KEY_USER_PW, null) ?: return null
-        val nickname = sharedPreferences.getString(KEY_NICKNAME, null) ?: return null
-        val mbti = sharedPreferences.getString(KEY_MBTI, null) ?: return null
+        val userId = preferences.getString(KEY_USER_ID, null) ?: return null
+        val password = preferences.getString(KEY_USER_PW, null) ?: return null
+        val nickname = preferences.getString(KEY_NICKNAME, null) ?: return null
+        val mbti = preferences.getString(KEY_MBTI, null) ?: return null
         return User(userId, password, nickname, mbti)
     }
 
     fun saveUserData(user: User) {
-        sharedPreferences.edit().apply {
+        preferences.edit().apply {
             putString(KEY_USER_ID, user.id)
             putString(KEY_USER_PW, user.password)
             putString(KEY_NICKNAME, user.nickname)
@@ -35,10 +30,10 @@ class UserRepository(context: Context){
     }
 
     fun clearUserData() {
-        sharedPreferences.edit().clear().apply()
+        preferences.edit().clear().apply()
     }
 
-    companion object{
+    companion object {
         private const val KEY_USER_ID = "UserID"
         private const val KEY_USER_PW = "Password"
         private const val KEY_NICKNAME = "Nickname"
