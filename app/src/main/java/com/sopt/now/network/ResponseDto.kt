c636c345257d04b7b2795e2681ba881406967476
0se0 <030ggong@gmail.com>
2024-05-01 02:39:08 +0900

    #18 Rename: 패키지 이동 및 이름 변경

--- app/src/main/java/com/sopt/now/network/ResponseDto.kt
+++ app/src/main/java/com/sopt/now/network/ResponseDto.kt
@@ -1,10 +1,10 @@
-package com.sopt.now.data.signup
+package com.sopt.now.network
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseSignUpDto(
+data class ResponseDto(
     @SerialName("code")
     val code: Int,
     @SerialName("message")
