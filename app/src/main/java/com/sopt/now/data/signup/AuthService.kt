18647a4d693cddfab22e46533b8a0a63abb6dc95
0se0 <030ggong@gmail.com>
2024-04-30 23:37:07 +0900

    #18 Feat: AuthService

---
+++ app/src/main/java/com/sopt/now/data/signup/AuthService.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.data.signup
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("member/join")
+    fun signUp(
+        @Body request: RequestSignUpDto,
+    ): Call<ResponseSignUpDto>
+}
