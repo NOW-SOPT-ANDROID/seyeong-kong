f35b95666dcab2e45cbf1f016c9c1459134437fe
0se0 <030ggong@gmail.com>
2024-05-02 01:19:01 +0900

    #18 Feat: FollowerApiFactory

--- app/src/main/java/com/sopt/now/network/FollowerApiFactory.kt
+++ app/src/main/java/com/sopt/now/network/FollowerApiFactory.kt
@@ -1,24 +1,19 @@
 package com.sopt.now.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.sopt.now.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
-object ApiFactory {
-    private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
+object FollowerApiFactory {
+    private const val BASE_URL: String = "https://reqres.in/api/"
 
-    private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HeaderInterceptor())
-        .build()
+    private val jsonConverterFactory = Json.asConverterFactory("application/json".toMediaType())
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .addConverterFactory(jsonConverterFactory)
             .build()
     }
 
