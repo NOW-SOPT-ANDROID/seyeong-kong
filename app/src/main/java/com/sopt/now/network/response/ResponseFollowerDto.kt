e373c9f301370ca12450eeae43a6d7b154cf8292
0se0 <030ggong@gmail.com>
2024-05-02 01:18:36 +0900

    #18 Feat: ResponseFollowerDto

---
+++ app/src/main/java/com/sopt/now/network/response/ResponseFollowerDto.kt
@@ -0,0 +1,34 @@
+package com.sopt.now.network.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseFollowerDto(
+    val page: Int,
+    @SerialName("per_page")
+    val perPage: Int,
+    val total: Int,
+    @SerialName("total_pages")
+    val totalPages: Int,
+    @SerialName("data")
+    val `data`: List<Data>,
+    val support: Support
+) {
+    @Serializable
+    data class Data(
+        val id: Int,
+        val email: String,
+        @SerialName("first_name")
+        val firstName: String,
+        @SerialName("last_name")
+        val lastName: String,
+        val avatar: String
+    )
+
+    @Serializable
+    data class Support(
+        val url: String,
+        val text: String
+    )
+}
