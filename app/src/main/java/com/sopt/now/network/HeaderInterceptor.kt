8bd238fd1c5c12ba7d267ccd37d5d08ab9dd1fe1
0se0 <030ggong@gmail.com>
2024-05-01 17:25:04 +0900

    #18 Feat: HeaderInterceptor 구현

---
+++ app/src/main/java/com/sopt/now/network/HeaderInterceptor.kt
@@ -0,0 +1,14 @@
+package com.sopt.now.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class HeaderInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val newRequest = originalRequest.newBuilder()
+            .addHeader("memberId", "223")
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
