9e77db9b081810f1b609eb77b75c0b1a1c680cf6
0se0 <030ggong@gmail.com>
2024-05-02 22:10:30 +0900

    #19 Feat: password 엔드포인트 추가

--- app/src/main/java/com/sopt/now/compose/data/network/AuthService.kt
+++ app/src/main/java/com/sopt/now/compose/data/network/AuthService.kt
@@ -1,10 +1,12 @@
 package com.sopt.now.compose.data.network
 
 import com.sopt.now.compose.data.network.reponse.ResponseDto
+import com.sopt.now.compose.data.network.request.RequestChPwDto
 import com.sopt.now.compose.data.network.request.RequestLoginDto
 import com.sopt.now.compose.data.network.request.RequestSignUpDto
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthService {
@@ -16,5 +18,10 @@ interface AuthService {
     @POST("member/login")
     fun login(
         @Body request: RequestLoginDto,
+    ): Call<ResponseDto>
+
+    @PATCH("member/password")
+    fun chPassword(
+        @Body request: RequestChPwDto,
     ): Call<ResponseDto>
 }
