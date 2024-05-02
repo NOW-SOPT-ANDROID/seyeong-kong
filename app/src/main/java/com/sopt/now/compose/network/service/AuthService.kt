bc03b39dafe1308c47a7ba65d69e52b13760d134
0se0 <030ggong@gmail.com>
2024-05-02 23:34:24 +0900

    #19 Rename: 패키지 변경에 따른 파일 추가

---
+++ app/src/main/java/com/sopt/now/compose/network/service/AuthService.kt
@@ -0,0 +1,27 @@
+package com.sopt.now.compose.network.service
+
+import com.sopt.now.compose.network.reponse.ResponseDto
+import com.sopt.now.compose.network.request.RequestChPwDto
+import com.sopt.now.compose.network.request.RequestLoginDto
+import com.sopt.now.compose.network.request.RequestSignUpDto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("member/join")
+    fun signUp(
+        @Body request: RequestSignUpDto,
+    ): Call<ResponseDto>
+
+    @POST("member/login")
+    fun login(
+        @Body request: RequestLoginDto,
+    ): Call<ResponseDto>
+
+    @PATCH("member/password")
+    fun chPassword(
+        @Body request: RequestChPwDto,
+    ): Call<ResponseDto>
+}
