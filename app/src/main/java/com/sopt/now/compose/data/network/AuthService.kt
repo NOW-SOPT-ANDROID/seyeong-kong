f5e0020e67bbe3f9addca65b4372342b79da276a
0se0 <030ggong@gmail.com>
2024-05-02 20:08:16 +0900

    #19 Feat: login 엔드 포인트

--- app/src/main/java/com/sopt/now/compose/data/network/AuthService.kt
+++ app/src/main/java/com/sopt/now/compose/data/network/AuthService.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.compose.data.network
 
 import com.sopt.now.compose.data.network.reponse.ResponseDto
+import com.sopt.now.compose.data.network.request.RequestLoginDto
 import com.sopt.now.compose.data.network.request.RequestSignUpDto
 import retrofit2.Call
 import retrofit2.http.Body
@@ -10,5 +11,10 @@ interface AuthService {
     @POST("member/join")
     fun signUp(
         @Body request: RequestSignUpDto,
+    ): Call<ResponseDto>
+
+    @POST("member/login")
+    fun login(
+        @Body request: RequestLoginDto,
     ): Call<ResponseDto>
 }
