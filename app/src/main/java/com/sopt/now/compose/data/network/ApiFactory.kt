8df8ccf974ef8df2cca5c74d7a11fbb414f968c1
0se0 <030ggong@gmail.com>
2024-05-02 22:11:06 +0900

    #19 Feat: okHttpClient 추가

--- app/src/main/java/com/sopt/now/compose/data/network/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/compose/data/network/ApiFactory.kt
@@ -4,14 +4,20 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import com.sopt.now.compose.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 object ApiFactory {
     private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HeaderInterceptor())
+        .build()
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
