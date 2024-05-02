093c1344ff6f553cba672c95a577185b38cdb246
0se0 <030ggong@gmail.com>
2024-05-02 22:11:56 +0900

    #19 Feat: ChPasswordScreen

---
+++ app/src/main/java/com/sopt/now/compose/ui/ch_password/ChPasswordScreen.kt
@@ -0,0 +1,158 @@
+package com.sopt.now.compose.ui.ch_password
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.sopt.now.compose.R
+import com.sopt.now.compose.data.network.request.RequestChPwDto
+import com.sopt.now.compose.util.noRippleClickable
+
+@Composable
+fun ChPasswordScreen(navController: NavController, viewModel: ChPasswordViewModel) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    var prePw by remember { mutableStateOf("") }
+    var newPw by remember { mutableStateOf("") }
+    var verifyPw by remember { mutableStateOf("") }
+
+    val authState by viewModel.liveData.observeAsState()
+
+    LaunchedEffect(authState) {
+        authState?.let { state ->
+            snackbarHostState.showSnackbar(
+                message = state.message,
+                duration = SnackbarDuration.Short
+            )
+            if (state.isSuccess) {
+                navController.navigate("login") {
+                    popUpTo("chPassword") { inclusive = true }
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        bottomBar = {}
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 20.dp, vertical = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                stringResource(R.string.ch_pw),
+                fontSize = 30.sp,
+            )
+
+            Spacer(Modifier.height(20.dp))
+            OutlinedTextField(
+                value = prePw,
+                onValueChange = { prePw = it },
+                label = { Text(stringResource(R.string.pre_pw)) },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(Modifier.height(16.dp))
+            OutlinedTextField(
+                value = newPw,
+                onValueChange = { newPw = it },
+                label = { Text(stringResource(R.string.new_pw)) },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(Modifier.height(16.dp))
+            OutlinedTextField(
+                value = verifyPw,
+                onValueChange = { verifyPw = it },
+                label = { Text(stringResource(R.string.verify_pw)) },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+            CustomBtn(
+                text = stringResource(R.string.btn_ch_pw),
+                onClick = {
+                    viewModel.chPassword(RequestChPwDto(prePw, newPw, verifyPw))
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun CustomBtn(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(30.dp)
+            )
+            .noRippleClickable(onClick = onClick)
+            .padding(vertical = 12.dp, horizontal = 16.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
