03b84f3ce1814ef846d8507452a7d71a56405d1a
0se0 <030ggong@gmail.com>
2024-05-02 19:30:08 +0900

    #19 Feat: NavGraphBuilder

---
+++ app/src/main/java/com/sopt/now/compose/ui/navigation/NavGraphBuilder.kt
@@ -0,0 +1,27 @@
+package com.sopt.now.compose.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.sopt.now.compose.data.User
+import com.sopt.now.compose.data.UserRepository
+import com.sopt.now.compose.ui.home.HomeScreen
+import com.sopt.now.compose.ui.login.LoginScreen
+import com.sopt.now.compose.ui.login.LoginViewModel
+import com.sopt.now.compose.ui.mypage.MypageScreen
+import com.sopt.now.compose.ui.search.SearchScreen
+import com.sopt.now.compose.ui.signup.SignupScreen
+import com.sopt.now.compose.ui.signup.SignupViewModel
+
+fun NavGraphBuilder.addNavGraph(
+    navController: NavHostController,
+    userRepository: UserRepository,
+    user: User?,
+    loginViewModel: LoginViewModel
+) {
+    composable("home") { HomeScreen() }
+    composable("search") { SearchScreen() }
+    composable("mypage") { MypageScreen(navController, userRepository, user) }
+    composable("login") { LoginScreen(navController, loginViewModel) }
+    composable("signup") { SignupScreen(navController) }
+}
