730b0d404488df8a2b3940828f360f2db4a8f583
0se0 <030ggong@gmail.com>
2024-05-02 19:29:50 +0900

    #19 Feat: NavGraph

---
+++ app/src/main/java/com/sopt/now/compose/ui/navigation/NavGraph.kt
@@ -0,0 +1,28 @@
+package com.sopt.now.compose.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.sopt.now.compose.data.User
+import com.sopt.now.compose.data.UserRepository
+import com.sopt.now.compose.ui.login.LoginViewModel
+import com.sopt.now.compose.ui.signup.SignupViewModel
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    userRepository: UserRepository,
+    user: User?,
+    loginViewModel: LoginViewModel
+) {
+    val startDestination = if (user != null || userRepository.isUserLoggedIn()) {
+        "home"
+    } else {
+        "login"
+    }
+    NavHost(navController = navController, startDestination = startDestination) {
+        addNavGraph(navController, userRepository, user, loginViewModel)
+    }
+}
+
