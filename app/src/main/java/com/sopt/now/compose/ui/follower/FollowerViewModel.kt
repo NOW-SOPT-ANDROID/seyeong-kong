2e6c37296ad7c223085ceb373e31fae04567edcc
0se0 <030ggong@gmail.com>
2024-05-02 23:35:55 +0900

    #19 Feat: FollowerViewModel

---
+++ app/src/main/java/com/sopt/now/compose/ui/follower/FollowerViewModel.kt
@@ -0,0 +1,40 @@
+package com.sopt.now.compose.ui.follower
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.sopt.now.compose.network.reponse.ResponseFollowerDto
+import com.sopt.now.compose.network.service.ServicePool
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FollowerViewModel : ViewModel() {
+    private val _followers = MutableStateFlow<List<ResponseFollowerDto.Data>>(emptyList())
+    val followers: StateFlow<List<ResponseFollowerDto.Data>> = _followers
+
+    init {
+        loadFollowers()
+    }
+
+    private fun loadFollowers() {
+        ServicePool.followerService.getFollowers(2).enqueue(object : Callback<ResponseFollowerDto> {
+            override fun onResponse(
+                call: Call<ResponseFollowerDto>,
+                response: Response<ResponseFollowerDto>,
+            ) {
+                if (response.isSuccessful) {
+                    _followers.value = response.body()?.data.orEmpty()
+                } else {
+                    Log.e("FollowerViewModel", "Failed to fetch followers")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseFollowerDto>, t: Throwable) {
+                Log.e("FollowerViewModel", "Error fetching followers", t)
+            }
+
+        })
+    }
+}
