439723264bce4ac6560d038b1d74c2d045ef621a
0se0 <030ggong@gmail.com>
2024-05-03 16:23:21 +0900

    #18 Feat: MypageViewModel

--- app/src/main/java/com/sopt/now/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/sopt/now/ui/mypage/MypageViewModel.kt
@@ -2,18 +2,42 @@ package com.sopt.now.ui.mypage
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.sopt.now.data.Sopt
-import com.sopt.now.data.User
+import com.sopt.now.Sopt.Companion.userRepository
+import com.sopt.now.network.response.ResponseInfoDto
+import com.sopt.now.network.response.UserInfo
+import com.sopt.now.network.service.ServicePool
+import com.sopt.now.ui.AuthState
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MypageViewModel: ViewModel() {
-    val userLiveData = MutableLiveData<User?>()
+    private val authService by lazy { ServicePool.authService }
+    val liveData = MutableLiveData<AuthState>()
+    val userLiveData = MutableLiveData<UserInfo>()
 
-    init {
-        userLiveData.value = Sopt.userRepository.getUserData()
+    fun info() {
+        authService.info().enqueue(object : Callback<ResponseInfoDto> {
+            override fun onResponse(call: Call<ResponseInfoDto>, response: Response<ResponseInfoDto>) {
+                if (response.isSuccessful) {
+                    response.body()?.data?.let {
+                        userLiveData.postValue(it)
+                    }
+                    liveData.value = AuthState(isSuccess = true, message = "회원 정보 조회 성공")
+                } else {
+                    liveData.value = AuthState(isSuccess = false, message = "회원 정보 조회 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseInfoDto>, t: Throwable) {
+                liveData.value = AuthState(isSuccess = false, message = "서버 에러")
+            }
+        })
     }
 
     fun logout() {
-        Sopt.userRepository.logoutUser()
-        userLiveData.value = null
+        userRepository.logoutUser()
     }
+
+
 }
