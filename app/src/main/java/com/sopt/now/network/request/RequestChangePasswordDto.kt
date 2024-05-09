9892a02db3660dd815495783894cfdf466fef278
0se0 <030ggong@gmail.com>
2024-05-09 16:34:11 +0900

    #18 Rename: 클래스 이름 변경

--- app/src/main/java/com/sopt/now/network/request/RequestChangePasswordDto.kt
+++ app/src/main/java/com/sopt/now/network/request/RequestChangePasswordDto.kt
@@ -3,7 +3,7 @@ package com.sopt.now.network.request
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RequestChangePwDto(
+data class RequestChangePasswordDto(
     val previousPassword: String,
     val newPassword: String,
     val newPasswordVerification: String
