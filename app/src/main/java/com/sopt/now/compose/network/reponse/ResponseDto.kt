ec1dd0bdce3d3e9475a70e09533080d40fba1f0f
0se0 <030ggong@gmail.com>
2024-05-10 13:26:05 +0900

    #19 Rename: SerialName 추가

--- app/src/main/java/com/sopt/now/compose/network/reponse/ResponseDto.kt
+++ app/src/main/java/com/sopt/now/compose/network/reponse/ResponseDto.kt
@@ -1,9 +1,12 @@
 package com.sopt.now.compose.network.reponse
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseDto(
+    @SerialName("code")
     val code: Int,
+    @SerialName("message")
     val message: String,
 )
