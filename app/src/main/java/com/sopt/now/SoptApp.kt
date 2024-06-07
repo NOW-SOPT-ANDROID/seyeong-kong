607f5496b90182a2dd7995ea33e3892c9fcaf359
0se0 <030ggong@gmail.com>
2024-06-07 17:13:15 +0900

    #27 Feat: serviceLocator에 따른 SoptApp 구현

--- app/src/main/java/com/sopt/now/SoptApp.kt
+++ app/src/main/java/com/sopt/now/SoptApp.kt
@@ -3,22 +3,21 @@ package com.sopt.now
 import android.app.Application
 import android.content.Context
 
-class SoptApp: Application() {
-    lateinit var appContainer: AppContainer
-
-    init {
-        instance = this
-    }
-    override fun onCreate() {
-        super.onCreate()
-        appContainer = AppContainer(applicationContext)
-    }
+class SoptApp : Application() {
+    lateinit var serviceLocator: ServiceLocator
 
     companion object {
-        private var instance: SoptApp? = null
+        @Volatile
+        private lateinit var instance: SoptApp
 
-        fun applicationContext() : Context {
-            return instance!!.applicationContext
+        fun applicationContext(): Context {
+            return instance.applicationContext
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        serviceLocator = ServiceLocator(applicationContext)
     }
 }
