9f1f09fae9a7ccc073a90c4b705ac6e5ce6c8c36
0se0 <030ggong@gmail.com>
2024-06-07 21:15:25 +0900

    #29 Feat: ChangePasswordViewModel runCatching으로 변경

--- app/src/main/java/com/sopt/now/compose/ui/changePassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/ui/changePassword/ChangePasswordViewModel.kt
@@ -2,11 +2,13 @@ package com.sopt.now.compose.ui.changePassword
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.SoptApp.Companion.userRepository
 import com.sopt.now.compose.network.service.ServicePool
 import com.sopt.now.compose.network.reponse.ResponseDto
 import com.sopt.now.compose.network.request.RequestChangePasswordDto
 import com.sopt.now.compose.ui.AuthState
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,34 +19,48 @@ class ChangePasswordViewModel : ViewModel() {
     val changePasswordStatus = MutableLiveData<AuthState>()
 
     fun changePassword(request: RequestChangePasswordDto) {
-        authService.changePassword(request).enqueue(object : Callback<ResponseDto> {
-            override fun onResponse(
-                call: Call<ResponseDto>,
-                response: Response<ResponseDto>,
-            ) {
-                if (response.isSuccessful) {
-                    val userId = response.headers()["location"]
-                    userRepository.updateUserPassword(request.newPassword)
-                    changePasswordStatus.value = AuthState(
-                        isSuccess = true,
-                        message = "비밀번호 변경 성공 유저의 ID는 $userId 입니다."
-                    )
-                } else {
-                    val rawJson = response.errorBody()?.string() ?: "No error message provided"
-                    val error = JSONObject(rawJson).optString("message", "error message")
-                    changePasswordStatus.value = AuthState(
-                        isSuccess = false,
-                        message = "비밀번호 변경 실패 $error"
-                    )
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseDto>, t: Throwable) {
+        viewModelScope.launch {
+            runCatching {
+                authService.changePassword(request)
+            }.onSuccess { response ->
+                handleSuccess(response, request)
+            }.onFailure {
                 changePasswordStatus.value = AuthState(
                     isSuccess = false,
                     message = "서버 에러"
                 )
             }
-        })
+        }
+    }
+
+    private fun handleSuccess(
+        response: Response<ResponseDto>,
+        request: RequestChangePasswordDto,
+    ) {
+        if (response.isSuccessful) {
+            successResponse(response, request)
+        } else {
+            failResponse(response)
+        }
+    }
+
+    private fun successResponse(
+        response: Response<ResponseDto>,
+        request: RequestChangePasswordDto,
+    ) {
+        val memberId = response.headers()["location"] ?: "unknown"
+        userRepository.updateUserPassword(request.newPassword)
+        changePasswordStatus.value = AuthState(
+            isSuccess = true,
+            message = "비밀번호 변경 성공 유저의 ID는 $memberId 입니다."
+        )
+    }
+
+    private fun failResponse(response: Response<ResponseDto>) {
+        val error = response.message()
+        changePasswordStatus.value = AuthState(
+            isSuccess = false,
+            message = "비밀번호 변경 실패 $error"
+        )
     }
 }
