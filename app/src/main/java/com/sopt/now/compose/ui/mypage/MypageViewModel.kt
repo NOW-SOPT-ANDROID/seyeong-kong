2c828b2f088cd72bc3e5208e6b0300c2fdf05a40
0se0 <030ggong@gmail.com>
2024-06-07 21:15:47 +0900

    #29 Feat: MypageViewModel runCatching으로 변경

--- app/src/main/java/com/sopt/now/compose/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/ui/mypage/MypageViewModel.kt
@@ -3,54 +3,65 @@ package com.sopt.now.compose.ui.mypage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.SoptApp.Companion.userRepository
 import com.sopt.now.compose.network.reponse.ResponseInfoDto
 import com.sopt.now.compose.network.reponse.UserInfo
 import com.sopt.now.compose.network.service.ServicePool
 import com.sopt.now.compose.ui.AuthState
-import retrofit2.Call
-import retrofit2.Callback
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class MypageViewModel : ViewModel() {
     private val authService by lazy { ServicePool.authService }
 
-    private val _mypageStatus = MutableLiveData<AuthState>()
+    private val _userInfoStatus = MutableLiveData<AuthState>()
     private val _userLiveData = MutableLiveData<UserInfo>()
     private val _successLogout = MutableLiveData<Boolean?>(null)
 
-    val mypageStatus: LiveData<AuthState> = _mypageStatus
+    val userInfoStatus: LiveData<AuthState> = _userInfoStatus
     val userLiveData: LiveData<UserInfo> = _userLiveData
     val successLogout: LiveData<Boolean?> = _successLogout
 
-    fun userInfo() {
-        authService.info().enqueue(object : Callback<ResponseInfoDto> {
-            override fun onResponse(
-                call: Call<ResponseInfoDto>,
-                response: Response<ResponseInfoDto>,
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.data?.let {
-                        _userLiveData.setValue(it)
-                    }
-                    _mypageStatus.value = AuthState(isSuccess = true, message = "회원 정보 조회 성공")
-                } else {
-                    _mypageStatus.value = AuthState(isSuccess = false, message = "회원 정보 조회 실패")
-                }
-            }
+    fun fetchUserInfo() {
+        viewModelScope.launch {
+            runCatching {
+                authService.userInfo()
+            }.onSuccess { response ->
+                handleSuccess(response)
+            }.onFailure {
+                _userInfoStatus.value = AuthState (
+                    isSuccess = false,
+                    message = "서버 에러"
+                )
 
-            override fun onFailure(call: Call<ResponseInfoDto>, t: Throwable) {
-                _mypageStatus.value = AuthState(isSuccess = false, message = "서버 에러")
             }
-        })
+        }
+    }
+
+    private fun handleSuccess(response: Response<ResponseInfoDto>) {
+        if (response.isSuccessful) {
+            successResponse(response)
+        } else {
+            failResponse()
+        }
+    }
+
+    private fun successResponse(response: Response<ResponseInfoDto>) {
+        response.body()?.data?.let { userInfo ->
+            _userLiveData.postValue(userInfo)
+            _userInfoStatus.postValue(AuthState(isSuccess = true, message = "회원 정보 조회 성공"))
+        } ?: run {
+            _userInfoStatus.postValue(AuthState(isSuccess = false, message = "회원 정보 없음"))
+        }
+    }
+
+    private fun failResponse() {
+        _userInfoStatus.value = AuthState(isSuccess = false, message = "회원 정보 조회 실패")
     }
 
     fun logout() {
-        if (userRepository.logoutUser()) {
-            userRepository.setUserLoggedIn(false)
-            _successLogout.value = true
-        } else {
-            _successLogout.value = false
-        }
+        userRepository.logoutUser()
+        _successLogout.postValue(true)
     }
 }
