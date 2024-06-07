4f0d7b91511bad211dad89dca9884a0359f68f0c
0se0 <030ggong@gmail.com>
2024-06-07 19:56:00 +0900

    #28 Feat: RepositoryModule

---
+++ app/src/main/java/com/sopt/now/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.sopt.now.di
+
+import com.sopt.now.data.UserRepository
+import com.sopt.now.data.UserRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(
+        impl: UserRepositoryImpl
+    ): UserRepository
+}
