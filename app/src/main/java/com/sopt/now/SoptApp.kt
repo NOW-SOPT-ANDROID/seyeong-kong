9c62ae24f587fce631cdaefde41232f67a9cb982
0se0 <030ggong@gmail.com>
2024-06-07 19:50:31 +0900

    #28 Feat: SoptApp

--- app/src/main/java/com/sopt/now/SoptApp.kt
+++ app/src/main/java/com/sopt/now/SoptApp.kt
@@ -1,23 +1,7 @@
 package com.sopt.now
 
 import android.app.Application
-import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
-class SoptApp : Application() {
-    lateinit var serviceLocator: ServiceLocator
-
-    companion object {
-        @Volatile
-        private lateinit var instance: SoptApp
-
-        fun applicationContext(): Context {
-            return instance.applicationContext
-        }
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-        serviceLocator = ServiceLocator(applicationContext)
-    }
-}
+@HiltAndroidApp
+class SoptApp : Application()
