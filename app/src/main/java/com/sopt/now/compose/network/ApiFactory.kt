e09b82d701e0da453b1c142cacd1c1a67b84bdf3
0se0 <030ggong@gmail.com>
2024-06-07 21:43:09 +0900

    #30 Feat: ServiceLocator에 따른 ApiFactory 변경

--- app/src/main/java/com/sopt/now/compose/network/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/compose/network/ApiFactory.kt
@@ -2,7 +2,7 @@ package com.sopt.now.compose.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sopt.now.compose.BuildConfig
-import com.sopt.now.compose.SoptApp.Companion.userRepository
+import com.sopt.now.compose.SoptApp
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -12,7 +12,7 @@ object ApiFactory {
     private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
 
     private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HeaderInterceptor(userRepository))
+        .addInterceptor(HeaderInterceptor(SoptApp.applicationContext()))
         .build()
 
     val retrofit: Retrofit by lazy {
