1eb41edde3239ebb6a222ca184e0031eaf6b789d
0se0 <030ggong@gmail.com>
2024-06-07 21:15:34 +0900

    #29 Feat: LoginViewModel runCatching으로 변경

--- app/src/main/java/com/sopt/now/compose/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/ui/login/LoginViewModel.kt
@@ -1,18 +1,15 @@
 package com.sopt.now.compose.ui.login
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.sopt.now.compose.SoptApp
+import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.SoptApp.Companion.userRepository
 import com.sopt.now.compose.network.service.ServicePool
 import com.sopt.now.compose.network.reponse.ResponseDto
 import com.sopt.now.compose.network.request.RequestLoginDto
 import com.sopt.now.compose.ui.AuthState
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class LoginViewModel : ViewModel() {
@@ -23,39 +20,43 @@ class LoginViewModel : ViewModel() {
     val loginStatus: LiveData<AuthState> = _loginStatus
 
     fun login(request: RequestLoginDto) {
-        authService.login(request).enqueue(object : Callback<ResponseDto> {
-            override fun onResponse(
-                call: Call<ResponseDto>,
-                response: Response<ResponseDto>,
-            ) {
-                if (response.isSuccessful) {
-                    val data: ResponseDto? = response.body()
-                    val memberId = response.headers()["location"] ?: "unknown"
-                    userRepository.setUserLoggedIn(true)
-                    userRepository.setMemberId(memberId)
-                    _loginStatus.value = AuthState(
-                        isSuccess = true,
-                        message = "로그인 성공 유저의 ID는 $memberId 입니다."
-                    )
-                    Log.d("Login", "data: $data, userId: $memberId")
-                } else {
-                    val statusCode = response.code()
-                    val rawJson = response.errorBody()?.string() ?: "No error message provided"
-                    val error = JSONObject(rawJson).optString("message", "error message")
-                    _loginStatus.value = AuthState(
-                        isSuccess = false,
-                        message = "로그인 실패 $error"
-                    )
-                    Log.d("LoginViewModel", "로그인 실패: HTTP Status Code: $statusCode, Error: $error")
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseDto>, t: Throwable) {
-                _loginStatus.value = AuthState(
+        viewModelScope.launch {
+            runCatching {
+                authService.login(request)
+            }.onSuccess { response ->
+                handleSuccess(response)
+            }.onFailure {
+                _loginStatus.value = AuthState (
                     isSuccess = false,
                     message = "서버 에러"
                 )
             }
-        })
+        }
+    }
+
+    private fun handleSuccess(response: Response<ResponseDto>) {
+        if (response.isSuccessful) {
+            successResponse(response)
+        } else {
+            failResponse(response)
+        }
+    }
+    private fun successResponse(response: Response<ResponseDto>) {
+        val memberId = response.headers()["location"] ?: "unknown"
+        userRepository.setUserLoggedIn(true)
+        userRepository.setMemberId(memberId)
+
+        _loginStatus.value = AuthState(
+            isSuccess = true,
+            message = "로그인 성공 유저의 ID는 $memberId 입니다."
+        )
+    }
+
+    private fun failResponse(response: Response<ResponseDto>) {
+        val error = response.message()
+        _loginStatus.value = AuthState(
+            isSuccess = false,
+            message = "로그인 실패 $error"
+        )
     }
 }
