91c9cc84a772b703e0b8a839dbb74f085affbe0a
0se0 <030ggong@gmail.com>
2024-06-07 19:58:59 +0900

    #28 Feat: object를 class로 변경 후 Hilt DI

--- app/src/main/java/com/sopt/now/network/FollowerApiFactory.kt
+++ app/src/main/java/com/sopt/now/network/FollowerApiFactory.kt
@@ -1,21 +1,12 @@
 package com.sopt.now.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import javax.inject.Inject
+import javax.inject.Singleton
 
-object FollowerApiFactory {
-    private const val BASE_URL: String = "https://reqres.in/api/"
-
-    private val jsonConverterFactory = Json.asConverterFactory("application/json".toMediaType())
-
-    val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(jsonConverterFactory)
-            .build()
-    }
-
+@Singleton
+class FollowerApiFactory @Inject constructor(
+    val retrofit: Retrofit
+) {
     inline fun <reified T> create(): T = retrofit.create(T::class.java)
 }
