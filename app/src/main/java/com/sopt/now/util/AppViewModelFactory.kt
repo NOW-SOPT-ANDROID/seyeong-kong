127b719d01079e75d70ab8c950aba84d997e0856
0se0 <030ggong@gmail.com>
2024-06-07 17:15:53 +0900

    #27 Feat: 기존 뷰모델 팩토리 삭제 후 AppViewModelFactory 구현

---
+++ app/src/main/java/com/sopt/now/util/AppViewModelFactory.kt
@@ -0,0 +1,35 @@
+package com.sopt.now.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sopt.now.data.UserRepository
+import com.sopt.now.data.friend.FriendsRepository
+import com.sopt.now.ui.home.HomeViewModel
+import com.sopt.now.ui.login.LoginViewModel
+import com.sopt.now.ui.mypage.MypageViewModel
+import com.sopt.now.ui.signup.SignupViewModel
+
+class AppViewModelFactory(
+    private val userRepository: UserRepository,
+    private val friendsRepository: FriendsRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(SignupViewModel::class.java) -> {
+                SignupViewModel(userRepository) as T
+            }
+            modelClass.isAssignableFrom(MypageViewModel::class.java) -> {
+                MypageViewModel(userRepository) as T
+            }
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(userRepository) as T
+            }
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                HomeViewModel(friendsRepository) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
