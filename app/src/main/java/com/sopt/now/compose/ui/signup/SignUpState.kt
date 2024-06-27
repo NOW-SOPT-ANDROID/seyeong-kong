69893fdbfd5f7dbb4f2bb3f8920265079a8e3373
0se0 <030ggong@gmail.com>
2024-06-27 22:39:35 +0900

    Feature #38: signupState

---
+++ app/src/main/java/com/sopt/now/compose/ui/signup/SignUpState.kt
@@ -0,0 +1,7 @@
+package com.sopt.now.compose.ui.signup
+data class SignUpState(
+    val id: String = "",
+    val password: String = "",
+    val nickname: String = "",
+    val phone: String = ""
+)
